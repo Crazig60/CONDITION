b69f0c4e995be0b2993407f445ddb967ae5e9518
Crazig60 <jerome.caouren@gmail.com>
2020-02-13 14:28:51 +0400

    serveur ajout

---
+++ medor.php
@@ -0,0 +1,24 @@
+<?php
+$Nom_animal = ($_POST['valeur']);
+    if ($Nom_animal == 'souris')
+        {
+            echo 'Médor est une petite souris'; 
+        }
+    elseif ($Nom_animal == 'girafe')
+            {
+                echo 'Médor est une girafe';
+            }
+        elseif ($Nom_animal == 'éléphant')
+                {
+                    echo 'Médor est un éléphant';
+                }
+            elseif ($Nom_animal == 'chien')
+                    {
+                        echo 'Médor est un chien';
+                    }
+                else
+                    {
+                        echo 'Non, non, non Médor n\'est pas un(e) '. $Nom_animal . ', mais un chien';
+                    }
+?>
+<p><a href="medor.htm">retour</a></p>
